a1a753e2e0958e3e169796e43e4bdead146f4d7e
Siera Ford <sieraford@Sieras-MacBook-Pro.local>
2022-11-27 08:35:41 -0800

    make spotify playlist uri s show up on page

--- assets/js/playlists.js
+++ assets/js/playlists.js
@@ -1,4 +1,5 @@
 var spotify_api_key = '1db2eac78emsha758dc391663482p1289b7jsn45dfc336be15'
+var playlists_container = document.getElementById("playlists-container")
 
 var getPlaylists = function() {
     const options = {
@@ -11,9 +12,33 @@ var getPlaylists = function() {
     
     var emotion = localStorage.getItem('emotion')
     fetch(`https://spotify23.p.rapidapi.com/search/?rapidapi-key=${spotify_api_key}&q=${emotion}&type=playlists&limit=5`, options)
-      .then(response => response.json())
-      .then(response => console.log(response.playlists.items))
-      .catch(err => console.error(err));
+    //   .then(response => response.json())
+    //   .then(response => 
+    //     console.log(response.playlists.items)
+    //     var playlist = document.createElement('div')
+    //     playlist.textContent = response.playlists.items
+    //     document.append(playlist)
+    //     )
+    //   .catch(err => console.error(err));
+
+
+      .then(function (response) {
+        if (response.ok) {
+          response.json().then(function (response) {
+            console.log(response.playlists.items);
+            for (i=0; i < 5; i++) {
+                var playlist = document.createElement('div')
+                playlist.textContent = response.playlists.items[i].data.uri
+                playlists_container.append(playlist)
+            }
+          });
+        } else {
+          alert('Error: ' + response.statusText);
+        }
+      })
+      .catch(function (error) {
+        alert('Cannot connect to Spoltify API');
+      });
   }
 
   getPlaylists();
