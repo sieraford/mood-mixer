93576054af8af6ea2bab7e3d65cc32a115afe37c
Siera Ford <sieraford@Sieras-MacBook-Pro.local>
2022-11-27 09:04:29 -0800

    add links to images and redirect user to playlist

--- assets/js/playlists.js
+++ assets/js/playlists.js
@@ -12,23 +12,20 @@ var getPlaylists = function() {
     
     var emotion = localStorage.getItem('emotion')
     fetch(`https://spotify23.p.rapidapi.com/search/?rapidapi-key=${spotify_api_key}&q=${emotion}&type=playlists&limit=5`, options)
-    //   .then(response => response.json())
-    //   .then(response => 
-    //     console.log(response.playlists.items)
-    //     var playlist = document.createElement('div')
-    //     playlist.textContent = response.playlists.items
-    //     document.append(playlist)
-    //     )
-    //   .catch(err => console.error(err));
-
 
       .then(function (response) {
         if (response.ok) {
           response.json().then(function (response) {
             console.log(response.playlists.items);
             for (i=0; i < 5; i++) {
-                var playlist = document.createElement('div')
-                playlist.textContent = response.playlists.items[i].data.uri
+                var playlist = document.createElement('a')
+                var playlistURI = response.playlists.items[i].data.uri
+                var playlistID = playlistURI.substring(17);
+                playlist.setAttribute('href', `https://open.spotify.com/playlist/${playlistID}`)
+                playlist.setAttribute('target', '_blank')
+                var imgURL = response.playlists.items[i].data.images.items[0].sources[0].url
+                var playlistName = response.playlists.items[i].data.name
+                playlist.innerHTML = '<img src = "'+imgURL+'"> '+playlistName+'';
                 playlists_container.append(playlist)
             }
           });
@@ -37,7 +34,7 @@ var getPlaylists = function() {
         }
       })
       .catch(function (error) {
-        alert('Cannot connect to Spoltify API');
+        alert('Cannot connect to Spotify API');
       });
   }
 
